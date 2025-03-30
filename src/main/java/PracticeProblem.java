252f4ac24ae1f2c1bbe24fc985b861417a29f006
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-03-30 22:38:58 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+//Create a method called insertionSort(char[] characters) that accepts and array of characters and sorts it using the insertion sort algorithm. Do not return anything.
+	public static void insertionSort(char[] characters){
+		for (int i = 1; i < characters.length; i++){
+			char key = characters[i];
+			int index = i - 1;
+			while (index >= 0 && key < characters[index]){
+				characters[index + 1] = characters[index];
+				index--;
+			}
+		characters [index + 1] = key;
+		}
+	}
 
 }
